26ae67a5fe0cac663adfbd560bdb9c043e7b2a6f
Tomasz Magulski <tomasz@magullab.io>
2016-12-08 18:22:54 +0100

    coherent name for vagrant and docker settings

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 
 wait-for-it db:5432
 
-python3 manage.py migrate --settings=volontulo_org.settings.docker
-python3 manage.py loaddata initial/data.json --settings=volontulo_org.settings.docker
-python3 manage.py runserver --settings=volontulo_org.settings.docker 0.0.0.0:8000
+python3 manage.py migrate --settings=volontulo_org.settings.dev_docker
+python3 manage.py loaddata initial/data.json --settings=volontulo_org.settings.dev_docker
+python3 manage.py runserver --settings=volontulo_org.settings.dev_docker 0.0.0.0:8000
