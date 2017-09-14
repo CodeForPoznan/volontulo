38131d1e44d288b297df927e41e2f720d232c4ed
Tomasz Jezierski <deco.jezierski@gmail.com>
2017-09-15 00:07:59 +0200

    Angular userservice login method (fixes #577)

---
+++ frontend/src/app/user.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http, Response, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+
+@Injectable()
+export class UserService {
+  private url = 'http://localhost:8000/api/login';
+
+  constructor (
+    private http: Http
+  ) {}
+
+  loginUser(username: string, password: string) {
+    let headers: Headers = new Headers({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    });
+    let options: RequestOptions = new RequestOptions({ headers: headers });
+
+    return this.http.post(this.url, { username, password }, options)
+    .map((res:Response) => res.json())
+    .catch(this.handleError);
+  }
+
+  handleError(reject: Response | any) {
+    const body = reject.json() || '';
+    const err = body.error || JSON.stringify(body);
+    console.log(err);
+    return Observable.throw(err);
+  }
+}
