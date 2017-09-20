aa6990cf6d8d9f710813a80bcae45fcc2ed61128
OtisRed <durczak.krzysztof@gmail.com>
2017-09-20 20:22:06 +0200

    Footer url in Angular (#632)

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { environment } from '../../environments/environment';
+
 
 @Component({
   selector: 'volontulo-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
 
-  constructor() { }
+  djangoRoot: string;
 
-  ngOnInit() {
+  constructor () {
+    this.djangoRoot = environment.djangoRoot;
   }
-
 }
