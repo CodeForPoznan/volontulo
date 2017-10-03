349d33d8bdb17020cbde401d70d4909dc916231f
Wojciech Patelka <wojciech.patelka@gmail.com>
2017-10-03 14:10:20 +0200

    [594] Angular tests for Header component (#679)

---
+++ frontend/src/app/header/header.component.spec.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { TestBed, inject, async } from '@angular/core/testing';
+import { HttpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { HeaderComponent } from './header.component';
+import { AuthService } from '../auth.service';
+
+
+describe('HeaderComponent', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        NgbModule.forRoot(),
+        HttpModule
+      ],
+      providers: [
+      ],
+      declarations: [
+        HeaderComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the header', async(() => {
+    inject([AuthService], () => {
+      const fixture = TestBed.createComponent(HeaderComponent);
+      const header = fixture.debugElement.componentInstance;
+      expect(header).toBeTruthy();
+    });
+  }));
+});
