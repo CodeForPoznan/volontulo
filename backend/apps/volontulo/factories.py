6f914a7d1cee734d8e6e1d7f751136ea31910abb
Rafał Kufel <rafal.kufel@gmail.com>
2017-12-20 20:41:37 +0100

    Offer factory plus test (#753)

--- backend/apps/volontulo/factories.py
+++ backend/apps/volontulo/factories.py
@@ -4,11 +4,15 @@
 .. module:: factories
 """
 
+import datetime
+
 from django.contrib.auth import get_user_model
+from django.utils import timezone
 import factory
 from factory.fuzzy import FuzzyChoice
 
-from apps.volontulo.models import Organization, UserProfile
+
+from apps.volontulo.models import Organization, UserProfile, Offer
 
 
 User = get_user_model()
@@ -20,6 +24,23 @@ class UserProfileFactory(factory.DjangoModelFactory):
     class Meta:  # pylint: disable=C0111
         model = UserProfile
 
+    user = factory.SubFactory(
+        "apps.volontulo.factories.UserFactory",
+        userprofile=None
+        )
+
+    @factory.post_generation
+    def organizations(self, create, extracted):
+        """Manage m2m relation."""
+        if not create:
+            return
+        if extracted:
+            for org in extracted:
+                self.organizations.add(org)
+
+    is_administrator = factory.fuzzy.FuzzyChoice(choices=[True, False])
+    phone_no = factory.Faker("phone_number", locale="pl_PL")
+
 
 class UserFactory(factory.DjangoModelFactory):
     """Factory for User."""
@@ -27,14 +48,14 @@ class UserFactory(factory.DjangoModelFactory):
     class Meta:  # pylint: disable=C0111
         model = User
 
-    first_name = factory.Faker('first_name', locale='pl_PL')
-    last_name = factory.Faker('last_name', locale='pl_PL')
-    email = factory.Faker('email', locale='pl_PL')
+    first_name = factory.Faker("first_name", locale="pl_PL")
+    last_name = factory.Faker("last_name", locale="pl_PL")
+    email = factory.Faker("email", locale="pl_PL")
     username = factory.LazyAttribute(lambda obj: obj.email)
 
     is_active = True
-    password = 'password123'
-    userprofile = factory.RelatedFactory(UserProfileFactory, 'user')
+    password = "password123"
+    userprofile = factory.RelatedFactory(UserProfileFactory, "user")
 
     @classmethod
     def _create(cls, model_class, *args, **kwargs):
@@ -49,57 +70,55 @@ class OrganizationFactory(factory.DjangoModelFactory):
         """Creates  a fake organization name.
 
         Fake name consist of predicate1 + subject + predicate2 + propername
-        np. 'Wojewódzka Alternatywa Organizacyjna "Naprzód"'.
+        np. "Wojewódzka Alternatywa Organizacyjna "Naprzód"".
         """
 
         predicate1_dict = {
-            'masculine': [
-                'Krajowy', 'Wojewódzki', 'Powiatowy', 'Regionalny',
-                'Wielkopolski', 'Osiedlowy', 'Stołeczny'],
-            'feminine': [
-                'Krajowa', 'Wojewódzka', 'Powiatowa', 'Regionalna',
-                'Wielkopolska', 'Osiedlowa', 'Stołeczna'],
-            'neutrum': [
-                'Krajowe', 'Wojewódzkie', 'Powiatowe', 'Regionalne',
-                'Wielkopolskie', 'Osiedlowe', 'Stołeczne']
+            "masculine": [
+                "Krajowy", "Wojewódzki", "Powiatowy", "Regionalny",
+                "Wielkopolski", "Osiedlowy", "Stołeczny"],
+            "feminine": [
+                "Krajowa", "Wojewódzka", "Powiatowa", "Regionalna",
+                "Wielkopolska", "Osiedlowa", "Stołeczna"],
+            "neutrum": [
+                "Krajowe", "Wojewódzkie", "Powiatowe", "Regionalne",
+                "Wielkopolskie", "Osiedlowe", "Stołeczne"]
             }
         noun_list = {
-            'Fundacja': 'feminine',
-            'Rada': 'feminine',
-            'Urząd': 'masculine',
-            'Zarząd': 'masculine',
-            'Delegatura': 'feminine',
-            'Poradnia': 'feminine',
-            'Szpital': 'masculine',
-            'Ogród': 'masculine',
-            'Koło': 'neutrum',
-            'Obwód': 'masculine'
+            "Fundacja": "feminine",
+            "Rada": "feminine",
+            "Urząd": "masculine",
+            "Zarząd": "masculine",
+            "Delegatura": "feminine",
+            "Poradnia": "feminine",
+            "Szpital": "masculine",
+            "Ogród": "masculine",
+            "Koło": "neutrum",
+            "Obwód": "masculine"
             }
         predicate2_dict = {
-            'masculine': [
-                'Organizacyjny', 'Rejestrowy', 'Egzekutywny', 'Wspierający',
-                'Transakcyjny', 'Związkowy', 'Zbiorczy'],
-            'feminine': [
-                'Organizacyjna', 'Rejestrowa', 'Egzekutywna', 'Wspierająca',
-                'Transakcyjna', 'Związkowa', 'Zbiorcza'],
-            'neutrum': [
-                'Organizacyjne', 'Rejestrowe', 'Egzekutywne', 'Wspierające',
-                'Transakcyjne', 'Związkowe', 'Zbiorcze']
+            "masculine": [
+                "Organizacyjny", "Rejestrowy", "Egzekutywny", "Wspierający",
+                "Transakcyjny", "Związkowy", "Zbiorczy"],
+            "feminine": [
+                "Organizacyjna", "Rejestrowa", "Egzekutywna", "Wspierająca",
+                "Transakcyjna", "Związkowa", "Zbiorcza"],
+            "neutrum": [
+                "Organizacyjne", "Rejestrowe", "Egzekutywne", "Wspierające",
+                "Transakcyjne", "Związkowe", "Zbiorcze"]
             }
 
         propername_list = [
-            '"Wspiera się"', '"Totuus"', '"Zawsze Razem"', '"W Kupie Siła"',
-            '"Al Capone"', '"UKF"', '"Smak Miesiąca"'
+            "Wspiera się", "Totuus", "Zawsze Razem", "W Kupie Siła",
+            "Al Capone", "UKF", "Smak Miesiąca"
             ]
-
-        # FuzzyChoice object
 
         subject = (FuzzyChoice(noun_list.keys())).fuzz()
         predicate1 = (FuzzyChoice(predicate1_dict[noun_list[subject]])).fuzz()
         predicate2 = (FuzzyChoice(predicate2_dict[noun_list[subject]])).fuzz()
         propername = (FuzzyChoice(propername_list)).fuzz()
 
-        return '{0} {1} {2} {3}'.format(
+        return "{0} {1} {2} {3}".format(
             predicate1,
             subject,
             predicate2,
@@ -110,5 +129,79 @@ class OrganizationFactory(factory.DjangoModelFactory):
         model = Organization
 
     name = factory.fuzzy.FuzzyAttribute(_organization_name)
-    address = factory.Faker('address', locale='pl_PL')
-    description = factory.Faker('paragraph', locale='pl_PL')
+    address = factory.Faker("address", locale="pl_PL")
+    description = factory.Faker("paragraph")
+
+
+class OfferFactory(factory.DjangoModelFactory):
+    """Factory for Offer"""
+
+    _end_date = timezone.now()
+    _start_date = _end_date + datetime.timedelta(days=-100)
+
+    class Meta:  # pylint: disable=C0111
+        model = Offer
+
+    organization = factory.SubFactory(OrganizationFactory)
+
+    @factory.post_generation
+    def volunteers(self, create, extracted):
+        """Manage ManyToMany field"""
+
+        if not create:
+            # Simple build, do nothing.
+            return
+
+        if extracted:
+            # A list of Users were passed in, use them
+            for user in extracted:
+                self.volunteers.add(user)
+
+    description = factory.Faker("paragraph")
+    requirements = factory.Faker("paragraph")
+    time_commitment = factory.Faker("paragraph")
+    benefits = factory.Faker("paragraph")
+    location = factory.Faker("address", locale="pl_PL")
+    title = factory.Faker("text", max_nb_chars=150)
+    started_at = factory.fuzzy.FuzzyDateTime(_start_date, _end_date)
+    finished_at = factory.fuzzy.FuzzyDateTime(_start_date, _end_date)
+    time_period = factory.Faker("text", max_nb_chars=150)
+    status_old = factory.fuzzy.FuzzyChoice(
+        choices=("NEW", "ACTIVE", "SUSPENDED")
+        )
+    offer_status = factory.fuzzy.FuzzyChoice(
+        choices=("Unpublished", "Published", "Rejected")
+        )
+    recruitment_status = factory.fuzzy.FuzzyChoice(
+        choices=("Open", "Supplemental", "Closed")
+        )
+    action_status = factory.fuzzy.FuzzyChoice(
+        choices=("Future", "Ongoing", "Finished")
+        )
+    votes = factory.fuzzy.FuzzyChoice(choices=(True, False))
+    recruitment_start_date = factory.fuzzy.FuzzyDateTime(
+        _start_date,
+        _end_date
+        )
+    recruitment_end_date = factory.fuzzy.FuzzyDateTime(
+        _start_date,
+        _end_date)
+    reserve_recruitment = factory.fuzzy.FuzzyChoice(choices=(True, False))
+    reserve_recruitment_start_date = factory.fuzzy.FuzzyDateTime(
+        _start_date,
+        _end_date
+        )
+    reserve_recruitment_end_date = factory.fuzzy.FuzzyDateTime(
+        _start_date,
+        _end_date
+        )
+    action_ongoing = factory.fuzzy.FuzzyChoice(choices=(True, False))
+    constant_coop = factory.fuzzy.FuzzyChoice(choices=(True, False))
+    action_start_date = factory.fuzzy.FuzzyDateTime(
+        _start_date,
+        _end_date
+        )
+    action_end_date = factory.fuzzy.FuzzyDateTime(_start_date, _end_date)
+    volunteers_limit = factory.fuzzy.FuzzyInteger(0, 1000)
+    reserve_volunteers_limit = factory.fuzzy.FuzzyInteger(0, 1000)
+    weight = factory.fuzzy.FuzzyInteger(0, 1000)
--- backend/apps/volontulo/tests/test_factories.py
+++ backend/apps/volontulo/tests/test_factories.py
@@ -2,47 +2,87 @@
 .. module:: test_factories
 """
 
+import datetime
+
 from django.contrib.auth.models import User
 from django.test import TestCase
 
-from apps.volontulo.factories import UserFactory, OrganizationFactory
-from apps.volontulo.models import Organization
+from apps.volontulo.factories import (
+    UserFactory, OrganizationFactory, OfferFactory, UserProfileFactory
+    )
+from apps.volontulo.models import Organization, Offer
 
 
 class UserFactoryTestCase(TestCase):
     """Test for UserFactory."""
     def setUp(self):
         """setting up each test."""
-        UserFactory.create(first_name='nie-Jan', last_name='nie-Kowalski')
+        UserFactory.create(first_name="nie-Jan", last_name="nie-Kowalski")
         self.totally_fake_user = UserFactory.create()
-        print(self.totally_fake_user)
 
     def test_factories_write_to_db(self):
         """Test if UserFactory properly create fake user."""
         self.assertEqual(User.objects.count(), 2)
 
     def test_UserFactory_firstname_lastname(self):
-        """test if UserFactory first_name fits to last_name."""
-        tested_user = User.objects.get(first_name='nie-Jan')
-        self.assertEqual(tested_user.last_name, 'nie-Kowalski')
+        """Test if UserFactory first_name fits to last_name."""
+        tested_user = User.objects.get(first_name="nie-Jan")
+        self.assertEqual(tested_user.last_name, "nie-Kowalski")
 
     def test_UserFactory_faker_if_first_last_name_is_str(self):
-        """test if created user.first_name,last_name is str and char>0."""
+        """Test if created user.first_name,last_name is str and char>0."""
         self.assertTrue(isinstance(self.totally_fake_user.first_name, str))
         self.assertTrue(isinstance(self.totally_fake_user.last_name, str))
         self.assertTrue(len(self.totally_fake_user.first_name) > 0)
         self.assertTrue(len(self.totally_fake_user.last_name) > 0)
 
     def test_userfactory_faker_if_email_has_at(self):
-        """test wether email of last created user contains an @. """
-        self.assertIn('@', self.totally_fake_user.email)
+        """Test if email of last created user contains an @. """
+        self.assertIn("@", self.totally_fake_user.email)
 
     def test_userfactory_faker_email_is_the_same_as_username(self):
-        """test if email equals user_name."""
+        """Test if email equals user_name."""
         self.assertEqual(
             self.totally_fake_user.email,
             self.totally_fake_user.username
             )
+
+
+class UserProfileFactoryTestCase(TestCase):
+    """Test for UserProfileFactory."""
+    def setUp(self):
+        OrganizationFactory.create(
+            name="Nazwa organizacji1",
+            )
+        OrganizationFactory.create()
+        # User is created as a Subfactory of UserProfile
+        self.fake_user_profile = UserProfileFactory.create(
+            user__first_name="Edmund",
+            organizations=Organization.objects.all(),
+            phone_no="22909"
+            )
+
+    def test_if_user_profile_has_been_created(self):
+        """Test if UserProfile has been created."""
+        self.assertEqual(self.fake_user_profile.phone_no, "22909")
+
+    def test_if_user_profile_is_connected_to_user(self):
+        """Test if UserProfile is connected to User."""
+        self.assertEqual(self.fake_user_profile.user.first_name, "Edmund")
+
+    def test_if_organizations_can_be_connected_with_User_Profile(self):
+        """Test m2m relation UserProfile-Organizations."""
+        organizations = self.fake_user_profile.organizations.all()
+        self.assertEqual(len(organizations), 2)
+        self.assertEqual(organizations[0].name, "Nazwa organizacji1")
+
+    def test_UserProfile_is_administrator_field(self):
+        """Test UserProfile is_administrator field."""
+        self.assertIsInstance(self.fake_user_profile.is_administrator, bool)
+
+    def test_UserProfil_phone_no_field(self):
+        """Test UserProfile phone_no field."""
+        self.assertFalse(self.fake_user_profile.phone_no.isalpha())
 
 
 class OrganizationFactoryTestCase(TestCase):
@@ -50,23 +90,166 @@ class OrganizationFactoryTestCase(TestCase):
     def setUp(self):
         """Set up each test"""
         OrganizationFactory.create(
-            name='Flota zjednoczonych sił',
-            address='Psia Wólka'
+            name="Flota zjednoczonych sił",
+            address="Psia Wólka"
             )
         self.fake_organization = OrganizationFactory.create()
 
     def test_organizationfactory(self):
-        """test if OrganizationFactory.create creates new Organization."""
+        """Test if OrganizationFactory.create creates new Organization."""
         test_organization = Organization.objects.get(
-            name='Flota zjednoczonych sił'
+            name="Flota zjednoczonych sił"
         )
-        self.assertEqual(test_organization.address, 'Psia Wólka')
+        self.assertEqual(test_organization.address, "Psia Wólka")
 
     def test_organization_if_name_address_description_is_str(self):
-        """test if organization name/address/description is str and char>0."""
+        """Test if organization name/address/description is str and char>0."""
         self.assertTrue(isinstance(self.fake_organization.name, str))
         self.assertTrue(isinstance(self.fake_organization.address, str))
         self.assertTrue(isinstance(self.fake_organization.description, str))
         self.assertTrue(len(self.fake_organization.name) > 0)
         self.assertTrue(len(self.fake_organization.address) > 0)
         self.assertTrue(len(self.fake_organization.description) > 0)
+
+
+class OfferFactoryTestCase(TestCase):
+    """Test for OfferFactory."""
+
+    def setUp(self):
+        """Set up test for OfferFactory"""
+
+        self.fake_user1 = UserFactory.create(
+            first_name="Fake user first_name1",
+            last_name="Fake user last_name1"
+            )
+        self.fake_user2 = UserFactory.create(
+            first_name="Fake user first_name2",
+            last_name="Fake user last_name2"
+            )
+        self.fake_offer1 = OfferFactory.create(volunteers=User.objects.all())
+        self.fake_offer2 = OfferFactory.create(
+            title="Jakiś tytuł",
+            description="Zwięzły opis",
+            organization__name="Nazwa odnośnej organizacji"
+            )
+        self.fake_offer3 = OfferFactory.create(
+            organization=Organization.objects.last()
+            )
+
+    def Test_if_users_have_been_created(self):
+        """Test if fake users have been created."""
+        self.assertTrue(len(User.objects.all()) == 2)
+
+    def Test_if_fake_organization_has_been_created(self):
+        """Test fake organization created by SubFactory."""
+        self.assertTrue(len(Organization.objects.all()) == 2)
+
+    def Test_if_offer_has_been_created(self):
+        """Test if offer has been created."""
+        created_offer = Offer.objects.get(title="Jakiś tytuł")
+        self.assertEqual(created_offer.description, "Zwięzły opis")
+
+    def Test_if_offer_is_connected_with_some_organization(self):
+        """Test if offer is connected with organization."""
+        fake_offer_with_organization = Offer.objects.filter(
+            title="Jakiś tytuł"
+            )[0]
+        self.assertTrue(
+            fake_offer_with_organization.organization.name,
+            "Nazwa odnośnej organizacji"
+            )
+
+    def Test_if_offer_is_connected_with_some_volonteer(self):
+        """Test if offer is connected with volunteer."""
+        fake_offer = Offer.objects.all()[0]
+        fake_offer_volunteers1 = fake_offer.volunteers.filter(
+            first_name="Fake user first_name1"
+            )
+        connected_user1 = fake_offer_volunteers1[0]
+        fake_offer_volunteers2 = fake_offer.volunteers.filter(
+            first_name="Fake user first_name2"
+            )
+        connected_user2 = fake_offer_volunteers2[0]
+        self.assertTrue(connected_user1.last_name, "Fake user last_name1")
+        self.assertTrue(connected_user2.last_name, "Fake user last_name2")
+
+    def test_offers_paragraph_is_str(self):
+        """Test if offers textFields are str and chr>0."""
+        description = self.fake_offer1.description
+        self.assertIsInstance(description, str)
+        self.assertTrue(len(description) > 0)
+        requirements = self.fake_offer1.requirements
+        self.assertIsInstance(requirements, str)
+        self.assertTrue(len(requirements) > 0)
+        time_commitment = self.fake_offer1.time_commitment
+        self.assertIsInstance(time_commitment, str)
+        self.assertTrue(len(time_commitment) > 0)
+        time_commitment = self.fake_offer1.time_commitment
+        self.assertIsInstance(time_commitment, str)
+        self.assertTrue(len(time_commitment) > 0)
+        title = self.fake_offer1.title
+        self.assertIsInstance(title, str)
+        self.assertTrue(len(title) > 0)
+        time_period = self.fake_offer1.time_period
+        self.assertIsInstance(time_period, str)
+        self.assertTrue(len(time_period) > 0)
+        location = self.fake_offer1.location
+        self.assertIsInstance(location, str)
+        self.assertTrue(len(location) > 0)
+
+    def test_offers_boolean_fields(self):
+        """Test if offers booleanFields are 0 or 1."""
+        self.assertIsInstance(self.fake_offer1.votes, bool)
+        self.assertIsInstance(self.fake_offer1.reserve_recruitment, bool)
+        self.assertIsInstance(self.fake_offer1.action_ongoing, bool)
+        self.assertIsInstance(self.fake_offer1.constant_coop, bool)
+
+    def test_offers_datatime_fields(self):
+        """Test if offers datatimeFields are proper type."""
+        self.assertIsInstance(self.fake_offer1.started_at, datetime.datetime)
+        self.assertIsInstance(self.fake_offer1.started_at, datetime.datetime)
+        self.assertIsInstance(
+            self.fake_offer1.recruitment_start_date,
+            datetime.datetime
+            )
+        self.assertIsInstance(
+            self.fake_offer1.reserve_recruitment_start_date,
+            datetime.datetime
+            )
+        self.assertIsInstance(
+            self.fake_offer1.reserve_recruitment_end_date,
+            datetime.datetime
+            )
+        self.assertIsInstance(
+            self.fake_offer1.action_start_date,
+            datetime.datetime
+            )
+        self.assertIsInstance(
+            self.fake_offer1.action_end_date,
+            datetime.datetime
+            )
+
+    def test_offer_if_choices_takes_proper_values(self):
+        """Test if fields with choices takes proper values."""
+        self.assertIn(
+            self.fake_offer1.status_old,
+            ["NEW", "ACTIVE", "SUSPENDED"]
+            )
+        self.assertIn(
+            self.fake_offer1.offer_status,
+            ["Unpublished", "Published", "Rejected"]
+            )
+        self.assertIn(
+            self.fake_offer1.recruitment_status,
+            ["Open", "Supplemental", "Closed"]
+            )
+        self.assertIn(
+            self.fake_offer1.action_status,
+            ["Future", "Ongoing", "Finished"]
+            )
+
+    def test_offer_if_integerField_are_proper_type(self):
+        """Testing if integerField is int type."""
+        self.assertIsInstance(self.fake_offer1.volunteers_limit, int)
+        self.assertIsInstance(self.fake_offer1.reserve_volunteers_limit, int)
+        self.assertIsInstance(self.fake_offer1.weight, int)
