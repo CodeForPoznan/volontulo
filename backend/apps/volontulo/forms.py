5eb9c20edeaed86937e67b99bf8489dbe74d97f9
ponycalypsenow <ponycalypsenow@gmail.com>
2018-01-20 13:40:15 +0100

    Offercreate date validation (fixes #725) (#748)

--- backend/apps/volontulo/forms.py
+++ backend/apps/volontulo/forms.py
@@ -89,6 +89,13 @@ class CreateOfferForm(forms.ModelForm):
 
     """Form reposponsible for creating offer by organization."""
 
+    start_finish_error = """Data rozpoczęcia akcji nie może być
+        późniejsza, niż data zakończenia"""
+    recruitment_error = """Data rozpoczęcia rekrutacji
+        nie może być późniejsza, niż data zakończenia"""
+    reserve_recruitment_error = """Data rozpoczęcia rekrutacji
+        rezerwowej nie może być późniejsza, niż data zakończenia"""
+
     def __init__(self, *args, **kwargs):
         super(CreateOfferForm, self).__init__(*args, **kwargs)
         self.fields['status_old'].required = False
@@ -119,6 +126,28 @@ class CreateOfferForm(forms.ModelForm):
             'volunteers_limit',
             'reserve_volunteers_limit',
         ]
+
+    def clean(self):
+        super(CreateOfferForm, self).clean()
+        self._clean_start_finish('started_at',
+                                 'finished_at',
+                                 self.start_finish_error)
+        self._clean_start_finish('recruitment_start_date',
+                                 'recruitment_end_date',
+                                 self.recruitment_error)
+        self._clean_start_finish('reserve_recruitment_start_date',
+                                 'reserve_recruitment_end_date',
+                                 self.reserve_recruitment_error)
+        return self.cleaned_data
+
+    def _clean_start_finish(self, start_slug, end_slug, error_desc):
+        """Validation for date fields."""
+        start_field_value = self.cleaned_data.get(start_slug)
+        end_field_value = self.cleaned_data.get(end_slug)
+        if start_field_value and end_field_value:
+            if start_field_value > end_field_value:
+                self.add_error(start_slug, error_desc)
+                self.add_error(end_slug, error_desc)
 
 
 class UserGalleryForm(forms.ModelForm):
