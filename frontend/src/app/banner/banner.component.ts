1fbde84e9b19e2369d83ee72e414b0ee9e4ff473
Przemysław Gosek <przemyslaw.gosek@stxnext.pl>
2018-04-11 20:07:43 +0200

    SCSS campaign: Refactor Banner SCSS (#911)

--- frontend/src/app/banner/banner.component.ts
+++ frontend/src/app/banner/banner.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'volontulo-banner',
-  templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+  templateUrl: './banner.component.html'
 })
 export class BannerComponent { }
