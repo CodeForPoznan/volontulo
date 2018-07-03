2c46345a80508e06e2a005900f046ed813d4f55a
Julia <julia.anholcer@interia.pl>
2018-07-03 23:16:43 +0200

    Account component/organizations (fixes #975) (#1009)

--- frontend/src/app/account/account.component.ts
+++ frontend/src/app/account/account.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
+
+import { AuthService } from '../auth.service';
+import { Organization } from '../organization/organization.model';
 
 @Component({
   selector: 'volontulo-account',
   templateUrl: './account.component.html',
 })
 
-export class AccountComponent {
+export class AccountComponent implements OnInit {
 
+  organizations$: Observable<Organization[]>;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.organizations$ = this.authService.user$
+      .pipe(
+        map(user => user.organizations)
+      );
+  }
 }
--- frontend/src/app/organizations/organizations-list/organizations-list.component.ts
+++ frontend/src/app/organizations/organizations-list/organizations-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+
 import { Organization } from '../../organization/organization.model';
 
 @Component({
